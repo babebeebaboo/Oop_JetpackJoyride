aa8b723de807a1f4e09aefc6bdecad12d114b651
BABE <supakorn.wo@ku.th>
2017-12-01 14:04:55 +0700

    DEL all unused code

--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -9,13 +9,10 @@ import com.badlogic.gdx.utils.Array;
 
 public class WorldRenderer {
     World world;
-    private Flyer flyer;
     private JetpackJoyrideGame jetpackjoyrideGame;
 
     private BitmapFont font;
     private Texture flyImg;
-    //private Texture atRoofImg;
-    private Texture runImg;
 
     //-- Animation CREDIT: http://www.pixnbgames.com/blog/libgdx/frame-by-frame-animations-in-libgdx/--//
     private TextureAtlas runningonRoofCharset,runningCharset;
@@ -25,8 +22,6 @@ public class WorldRenderer {
     private TextureRegion runningFrame;
     private TextureRegion runningonRoofFrame;
     private float elapsed_time = 0f;
-    //private float running_origin_x, running_origin_y;
-    //private float onRoof_origin_x, onRoof_origin_y;
     //--END Animation --//
 
     public WorldRenderer(JetpackJoyrideGame jetpackjoyrideGame, World world) {
@@ -34,26 +29,18 @@ public class WorldRenderer {
         SpriteBatch batch = jetpackjoyrideGame.batch;
         this.world = world;
         flyImg = new Texture("flyer.png");
-        runImg = new Texture("runner.png");
-        //atRoofImg = new Texture("runnerOnRoof.png");
         font = new BitmapFont();
 
         //--- Running Animation ---//
         runningCharset = new TextureAtlas( Gdx.files.internal("running.atlas") );
         Array<TextureAtlas.AtlasRegion> runningFrames = runningCharset.findRegions("running");
         runningAnimation = new Animation(FRAME_DURATION, runningFrames, Animation.PlayMode.LOOP);
-        TextureRegion runningTexture = runningFrames.first();
-        //running_origin_x = (Gdx.graphics.getWidth()  - runningTexture.getRegionWidth())  / 2;
-        //running_origin_y = (Gdx.graphics.getHeight() - runningTexture.getRegionHeight()) / 2;
         //--- END Running Animation ---//
 
         //--- Running Roof Animation ---//
         runningonRoofCharset = new TextureAtlas( Gdx.files.internal("runningonRoof.atlas") );
         Array<TextureAtlas.AtlasRegion> runningonRoofFrames = runningonRoofCharset.findRegions("running");
         runningonRoofAnimation = new Animation(FRAME_DURATION, runningonRoofFrames, Animation.PlayMode.LOOP);
-        TextureRegion runningonRoofTexture = runningonRoofFrames.first();
-        //onRoof_origin_x = (Gdx.graphics.getWidth()  - runningonRoofTexture.getRegionWidth())  / 2;
-        //onRoof_origin_y = (Gdx.graphics.getHeight() - runningonRoofTexture.getRegionHeight()) / 2;
         //--- END Running Roof Animation ---//
 
     }
@@ -63,7 +50,7 @@ public class WorldRenderer {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         world.update(delta);
         SpriteBatch batch = jetpackjoyrideGame.batch;
-
+        Flyer flyer = world.getFlyer();
         Vector2 pos = world.getFlyer().getPosition();
         batch.begin();
 
